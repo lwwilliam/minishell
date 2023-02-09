6e32c93ef6dfbbb7fb19d41751347cf235d058ea
Lee William <lwilliam@u90z03s05.42kl.edu.my>
2023-02-09 22:20:35 +0800

    ajkbdawd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lwilliam <lwilliam@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 16:28:24 by lwilliam          #+#    #+#             */
-/*   Updated: 2023/02/09 21:33:07 by lwilliam         ###   ########.fr       */
+/*   Updated: 2023/02/09 22:19:59 by lwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	env_init(char **in, t_minihell *mini)
 	int	x;
 
 	x = 0;
-	mini = (t_minihell *)malloc(sizeof(t_minihell));
 	while (in[x])
 		x++;
-	mini->env_arr = malloc(sizeof(char *) * (x + 1));
+	mini->env_arr = (char **)ft_calloc(x + 1, sizeof(char *));
 	mini->env_arr[x] = NULL;
-	while (--x >= 0)
+	x = -1;
+	while (in[++x])
 		mini->env_arr[x] = ft_strdup(in[x]);
 	return (0);
 }
@@ -38,18 +38,19 @@ int	main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	env_init(env, &mini);
-	// pwd = getcwd(NULL, 1024);
 	while (1)
 	{
 		printf("%s", "Minishell : ");
 		t = readline(0);
 		printf("%s\n", t);
-		// if (!ft_strncmp(t, "env", 3))
-		// {
-		// 	x = 0;
-		// 	while (mini.env_arr[x++])
-		// 		printf("%s\n", mini.env_arr[x]);
-		// }
+		if (!ft_strncmp(t, "env", 3))
+		{
+			x = 0;
+			while (mini.env_arr[x])
+				printf("%s\n", mini.env_arr[x++]);
+		}
+		if (!ft_strncmp(t, "pwd", 3))
+			printf("%s\n", getcwd(NULL, 1024));
 		if (!ft_strncmp(t, "exit", 4))
 			return (69);
 	}
