2d9d54419690813a916696937eeec79f81c88796
Wallace Ting <wting@u90z03s03.42kl.edu.my>
2023-03-07 13:06:17 +0800

    moving

--- hell_seperate.c
+++ hell_seperate.c
@@ -6,8 +6,51 @@
 /*   By: wting <wting@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 16:42:06 by wting             #+#    #+#             */
-/*   Updated: 2023/02/23 21:46:26 by wting            ###   ########.fr       */
+/*   Updated: 2023/03/07 13:05:49 by wting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	sep_char(char **str, char seperator)
+{
+	
+}
+
+int	find_set(char c)
+{
+	char	*charset;
+	int		i;
+	
+	charset = "><|";
+	i = 0;
+	if (!charset || !c)
+		return (0);
+	while (charset[i])
+	{
+		if (charset[i] == c)
+			return (charset[i]);
+		++i;
+	}
+	return (0);
+}
+
+char	*seperate(char *str, t_minihell mini)
+{
+	int		i;
+	
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '\'')
+			while (str[++i] && str[i] != '\'')
+				;
+		if (str[i] == '"')
+			while (str[++i] && str[i] != '"')
+				;
+		if (findset(str[i]))
+		{
+			
+		}
+	}
+}
