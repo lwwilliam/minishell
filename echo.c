66c22d7601db9bca9c0040cd30fa36744d2cfc27
lwwilliam <lee1052william@gmail.com>
2023-03-22 18:53:52 +0800

    fix echo with redir issues

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: lwilliam <lwilliam@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 00:04:37 by lwilliam          #+#    #+#             */
-/*   Updated: 2023/03/16 13:31:24 by lwilliam         ###   ########.fr       */
+/*   Updated: 2023/03/22 18:51:46 by lwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 void	echo(t_minihell *mini)
 {
 	int		x;
+	char	*tmp;
+	char	*line;
 
 	x = 0;
+	line = "";
 	while (mini->input_arr[++x])
 	{
 		if (!ft_strncmp(mini->input_arr[x], "-n", 2))
@@ -27,9 +30,16 @@ void	echo(t_minihell *mini)
 			|| !ft_strncmp(mini->input_arr[x], ">>", 3))
 			break ;
 		else if (mini->input_arr[x] != 0)
-			printf("%s ", mini->input_arr[x]);
+		{
+			tmp = ft_strjoin(line, mini->input_arr[x]);
+			line = ft_strjoin(tmp, " ");
+		}
 	}
 	if (ft_strncmp(mini->input_arr[1], "-n", 2))
-		printf("\n");
-	printf("\b");
+	{
+		free(tmp);
+		tmp = ft_strjoin(line, "\n");
+	}
+	write(1, tmp, ft_strlen(tmp));
+	free(line);
 }
